5a3b7dd7ee7cc552cd5afef7d5053c2885c9d89d
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-10-11 23:06:22 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main';
+import Statistics from './Components/Statistics/Statistics';
+import Blog from './Components/Blogs/Blog';
+import About from './Components/Abouts/About';
+
 
 function App() {
   const router = createBrowserRouter([
     {
       path:'/',
-      element:
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/home',
+          element:<Home></Home>
+        },
+        {
+        path:'statistics', 
+        element: <Statistics/>
+        },
+
+        {
+        path: 'blog',
+        element: <Blog/>
+        },
+
+        {
+          path: 'about',
+          element: <About/>
+          }
+        
+
+      ]
     }
+    
 
   ])
   return (
--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Blog = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <h1>this is blog page</h1>
         </div>
     );
 };
 
-export default Home;
+export default Blog;
