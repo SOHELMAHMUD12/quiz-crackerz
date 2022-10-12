a181909d05eef8509a504ec2d65fffaef8a8f646
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-10-12 15:56:49 +0600

    added about us

--- src/Components/Abouts/About.js
+++ src/Components/Abouts/About.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>this is about page</h2>
+        <div className='container'>
+            <h1 className='text-2xl'>Introduction</h1>
+Online quizzes are a popular form of entertainment for web surfers. Online quizzes are generally free to play and for entertainment purposes only though some online quiz websites offer prizes. Websites feature online quizzes on many subjects. One popular type of online quiz is a personality quiz or relationship quiz which is similar to what can be found in many women's or teen magazines. Websites hosting quizzes include Quizilla, FunTrivia, OkCupid, Sporcle, and Quizlet.
+
+<h1 className='text-2xl'>Blog quizzes</h1>
+Blog quizzes (also known as quiz blog) refer to a specific genre of quizzes which are conducted by the quizzers on blogs.[citation needed] Blog quizzes may be about verbs[1] or a wide range of other topics.
+
+<h1 className='text-2xl'>Educational quizzes</h1>
+Quiz is one of the most common eLearning patterns for many of the online course. Some companies and schools use online quizzes as a means to educate their employees or students respectively. Popular websites hosting quizzes for this purpose include Quizlet and Revision Quiz Maker.
+
+
+<h1 className='text-2xl'>Practical applications</h1>
+Many online quizzes are set up to actually test knowledge or identify a person's attributes. Some companies use online quizzes as an efficient way of testing a potential hire's knowledge without that candidate needing to travel. Online dating services often use personality quizzes to find a match between similar members.
+
+<h1 className='text-2xl'>Other</h1>
+Most online quizzes are to be taken lightly. The results do not often reflect the true personality or relationship. They are also rarely psychometrically valid. However, they may occasion reflection on the subject of the quiz and provide a springboard for a person to explore his or her emotions, beliefs, or actions.
+
+
         </div>
     );
 };
